1dcc5d9baccdb30f2d85dd5feccde2ab859fa441
Yazan Almoued <yazanmawed98@gmail.com>
2025-04-14 13:11:54 +0200

    Update Nash_To_Sim.py

--- Nash_To_Sim.py
+++ Nash_To_Sim.py
@@ -1,5 +1,7 @@
 import numpy as np
 import nashpy as nash
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from mesa import Model, Agent
 from mesa.time import RandomActivation
